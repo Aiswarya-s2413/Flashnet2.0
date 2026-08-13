95a0571df1d041d8b3cdf3cdbc13c95e844e653c
Aiswarya <aiswarya@users.noreply.github.com>
2026-08-13 15:36:29 +0530

    Increase Django max file upload limit to 100MB

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -178,5 +178,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = env('EMAIL_HOST', default='smtp.gmail.com')
 EMAIL_PORT = env.int('EMAIL_PORT', default=587)
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env('EMAIL_HOST_USER', default='')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', default='')
+
+# File upload size limit (100MB)
+DATA_UPLOAD_MAX_MEMORY_SIZE = 104857600
+FILE_UPLOAD_MAX_MEMORY_SIZE = 104857600
